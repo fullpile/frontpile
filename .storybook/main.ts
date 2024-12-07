ef55432c07dd350153811ed8787c6dcbf83ca3d8
Amine Azizi <hi@amineazizi.com>
2024-12-07 20:09:08 +0100

    Enhancing the storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,7 +16,6 @@ const config: StorybookConfig = {
       builder: {
         useSWC: true,
       },
-      // enable React strict mode
       strictMode: true,
     },
   },
@@ -24,36 +23,22 @@ const config: StorybookConfig = {
     jsc: {
       transform: {
         react: {
-          // Do not require importing React into scope to use JSX
           runtime: 'automatic',
         },
       },
     },
   }),
 
-  // we need to add aliases to webpack so it knows how to follow
-  // to the source of the packages rather than the built version (dist)
-  webpackFinal: async (config) => ({
-    ...config,
-    resolve: {
-      ...config.resolve,
-      alias: {
-        ...config.resolve?.alias,
-        ...convertTsConfigPathsToWebpackAliases(),
-      },
-    },
-  }),
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@frontpile/*': path.resolve(__dirname, '../packages/elements/*/src'),
+        '@frontpile/variants/*': path.resolve(__dirname, '../packages/variants/*/src'),
+      };
+    }
+    return config;
+  },
 };
-
-function convertTsConfigPathsToWebpackAliases() {
-  const rootDir = path.resolve(__dirname, '../');
-  const tsconfig = require('../tsconfig.json');
-  const tsconfigPaths: Array<string | string[]> = Object.entries(tsconfig.compilerOptions.paths);
-
-  return tsconfigPaths.reduce((aliases, [realPath, mappedPath]) => {
-    aliases[realPath] = path.join(rootDir, mappedPath[0]);
-    return aliases;
-  }, {});
-}
 
 export default config;
